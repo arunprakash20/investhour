8f95893be0eb634969c82d7f3fdd62c045afb3c8
apthambu20 <apthambu20@gmail.com>
2026-08-19 01:02:24 +0530

    Set investhour.in as production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://investhour.pages.dev',
+  site: 'https://investhour.in',
   integrations: [sitemap()],
 });
