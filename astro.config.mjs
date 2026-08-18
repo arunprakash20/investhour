cfb29c2895f93e1612b711a9653ef7b8c9b5047e
apthambu20 <apthambu20@gmail.com>
2026-08-18 23:19:06 +0530

    Configure production sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://investhour.pages.dev',
   integrations: [sitemap()],
 });
